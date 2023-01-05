ee91ca67c791baa4c9703c9b5fa0e083a4ed59ad
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-05 11:39:11 -0300

    criei a rota get

--- backend/src/controller/clienteController.js
+++ backend/src/controller/clienteController.js
@@ -3,6 +3,10 @@ const clienteController = app =>{
     app.post('/cliente', (req, res)=>{
         res.send("rota post")
     })
+
+    app.get('/cliente', (req, res)=>{
+        res.send("rota get")
+    })
 }
 
 export default clienteController
