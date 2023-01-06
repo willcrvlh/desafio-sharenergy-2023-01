6e52c2b2761c914cd54511fa38a4b65c7511c33c
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-06 11:13:40 -0300

    importei o react router dom na app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import Header from './Components/Header';
+import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 
 function App() {
 
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+      <Switch>
+
+      </Switch>
+      </BrowserRouter>
     </div>
   )
 }
