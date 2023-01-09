3863a1e5e5f3227a47ebcba5b86f782a366a82a3
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 12:02:08 -0300

    importei o react-roouter-dom para criar a rota das paginas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
-import './App.css'
-import Header from './Components/Header';
+import Header from '../src/Components/Header/Header';
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Usuarios from './Pages/Usuarios/Usuarios'
+import Footer from './Components/Footer/Footer';
 
 function App() {
 
@@ -9,9 +11,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header />
-      <Switch>
-
-      </Switch>
+        <Switch>
+          <Route path="" exact element={<Home/>}/>
+          <Route path="/usuarios" exact element={<Usuarios/>}/>
+        </Switch>
+        <Footer/>
       </BrowserRouter>
     </div>
   )
