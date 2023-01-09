04ddfad9fa92d58ee635e1ffc676d294912ba236
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 19:56:30 -0300

    utilizei o boostrap no header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import { H1 ,  } from './style';
-import img  from '../../assets/share.png'
-import S from './Header.module.css';
+import React from "react";
+import { H1 } from "./style";
+import img from "../../assets/share.png";
+import S from "./Header.module.css";
 function Header() {
-    return (
-        <div className={S.header}>
-            <div className={S.foto}>
-                <a href="/"><img src={img} alt="Logo da empresa" className={S.logo}/>
+  return (
+    <div className={S.header}>
+      <div div class="container-fluid" className={S.foto}>
+        <a class="navbar-brand" href="#">
+          <img
+            src={img}
+            alt="Logo"
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+            className={S.logo}
+          />
+        </a>
+          <H1>Sharenergy</H1>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarNav" className={S.div}>
+        <ul class="navbar-nav" className={S.ul}>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/Usuarios">
+              Usuarios
             </a>
-            <H1>Sharenergy</H1>
-            </div>
-            <div className={S.div}>
-                <ul className={S.ul}>
-                    <li><a href="/usuarios">Usuarios</a></li>
-                    <li> <a href="">HTTP Cat</a></li>
-                    <li><a href="">Random Dog</a></li>
-                    <li><a href="">Clientes</a></li>
-                </ul>
-            </div>
-        </div>
-    )
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="">
+              HTTP Cat
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Randon Dog
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Clientes
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
