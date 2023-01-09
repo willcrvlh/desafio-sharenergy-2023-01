58adf62add7b988cbe7532099a63db35eba46817
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 15:23:24 -0300

    fiz o estilo para o header

---
+++ frontend/src/Components/Header/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+    color: #515151;
+    font-family: 'Montserrat', sans-serif;
+    margin-top: 0.56em;
+`
+
+export const img =styled.img`
+    width: 350px;
+`
