35838f12c3fbfa034d1e253d3aefe1488bf95b7f
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 22:54:48 -0300

    fiz o componente card cliente

--- frontend/src/Components/CardClientes/CardClientes.jsx
+++ frontend/src/Components/CardClientes/CardClientes.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import S from './CardCliente.module.css'
 function CardClientes() {
   return (
-    <div>
-        
+    <div className={S.div}>
+        <h3 className={S.nome}>Nome</h3>
+        <h4 className={S.email}>Email</h4>
+        <p className={S.p}>Telefone</p>
+        <p className={S.p}>Endereço</p>
+        <p className={S.p}>CPF</p>
     </div>
   )
 }
