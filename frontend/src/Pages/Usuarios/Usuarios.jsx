4092efb202ca8881fde9541cefcccd789b59e9e0
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 22:30:50 -0300

    fiz a pagina de usuarios e o css dela

--- frontend/src/Pages/Usuarios/Usuarios.jsx
+++ frontend/src/Pages/Usuarios/Usuarios.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import Button from '../../Components/Button/Button'
+import Button from '../../Components/Buttons/Buttons'
+import Card from '../../Components/Card/Card'
+import Input from '../../Components/Input/Input'
+import S from './Usuarios.module.css'
 
 
 function Usuarios() {
   return (
-    <div>
-      <Button/>
+    <div className={S.div}>
+      <h1 className={S.h1}>BUSCA USUARIOS</h1>
+      <div className={S.busca}>
+        <Input texto="Digite um Usuario" className={S.input}/>
+        <Button text="Buscar"/>
+      </div>
+      <Card />
     </div>
   )
 }
