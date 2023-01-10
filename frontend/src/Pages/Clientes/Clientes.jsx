75ee0f0049f635606db3886359fd222533c3bd93
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2023-01-09 22:55:47 -0300

    importei o card cliente na pagina cliente

--- frontend/src/Pages/Clientes/Clientes.jsx
+++ frontend/src/Pages/Clientes/Clientes.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import CardClientes from '../../Components/CardClientes/CardClientes'
+import S from './Cliente.module.css'
 function Clientes() {
   return (
-    <div>
-        <h1>clientes</h1>
+    <div >
+        <h1 className={S.h1}>Clientes</h1>
+        <CardClientes/>
     </div>
   )
 }
